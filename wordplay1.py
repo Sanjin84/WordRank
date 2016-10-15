ab8cadf05dfc15f9bb3ca0a6e89806cab6a7f923
crazycreeper064 <amaiamcooper@gmail.com>
2016-10-15 17:43:42 +1100

    Add files via upload

---
+++ wordplay1.py
@@ -0,0 +1,38 @@
+trump = '''
+1. Arab-Americans cheered the attacks on 9/11. Trump repeatedly claimed
+that on 11 September, 2001, there were thousands of Arab-Americans
+celebrating in New Jersey after two planes flew into the Twin Towers. He
+says such public demonstrations "tell you something" about Muslims
+living in the US. However, there are no media reports to back up the
+claim.
+2. There should be surveillance on US mosques. Trump believes Muslims
+should be tracked by law enforcement as a counterterrorism initiative.
+He has walked back some comments about keeping a database on all
+American Muslims, but says he doesn't care if watching mosques is seen
+as "politically incorrect".ds
+
+'''
+counter = 0
+trump = trump.split()
+nrep = [ ]
+dword = [ ]
+print(trump)
+print trump[3]*3
+length = len(trump)
+for i in range(0,length):
+    if (trump[i]) not in nrep:
+        nrep.append(trump[i])
+    else:
+        print (trump[i])
+        dword.append(trump[i])
+        if (trump[i]) in dword:
+            print  (trump[i])
+            
+
+ 
+    
+
+
+
+
+
